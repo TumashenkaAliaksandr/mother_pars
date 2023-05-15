5cd21b4aec230fd05fbe95bed38edde83b2e083f
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-05-16 01:25:56 +0300

    Continuing parsing products on solar, added category inversion, done

--- tests/test_pars.py
+++ tests/test_pars.py
@@ -1,3 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+FILENAME = 'urls_carador_cohe.csv'
+
+
+def create_csv(filename, fieldnames):
+    with open(filename, 'w', encoding='utf-8', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+
+def write_csv(filename, data, fieldnames):
+    with open(filename, 'a', encoding='utf-8', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(data)
+
+
+# Отправка запроса на страницу категории
+url = "https://autosolar.es/cargador-coche-electrico"
+response = requests.get(url)
+
+# Проверка успешности запроса
+if response.status_code == 200:
+    # Инициализация объекта BeautifulSoup для парсинга HTML
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Нахождение всех ссылок на товары
+    product_links = set()
+    link_elements = soup.select('div.category-products a')
+    for link_element in link_elements:
+        link = link_element.get("href")
+        product_links.add(link)
+
+    # Поиск и обработка остальных страниц пагинации
+    pagination = soup.find("div", {'class': 'pagination'})
+    if pagination is not None:
+        pages = pagination.find_all("li")
+        for page in pages:
+            page_link = page.find("a")
+            if page_link is not None:
+                page_url = page_link["href"]
+                page_response = requests.get(page_url)
+                if page_response.status_code == 200:
+                    page_soup = BeautifulSoup(page_response.content, "html.parser")
+                    page_link_elements = page_soup.select('div.category-products a')
+                    for page_link_element in page_link_elements:
+                        page_link = page_link_element.get("href")
+                        product_links.add(page_link)
+
+    # Запись ссылок на товары в файл
+    fieldnames = ['url']
+    create_csv(FILENAME, fieldnames)
+    for link in product_links:
+        data = {'url': link}
+        write_csv(FILENAME, data, fieldnames)
+else:
+    print("Не удалось выполнить запрос к странице категории.")
+
+
+
 # import requests
 # from bs4 import BeautifulSoup
 #
@@ -133,65 +195,3 @@ import csv
 # else:
 #     print("Не удалось выполнить запрос к странице категории.")
 #
-
-import requests
-from bs4 import BeautifulSoup
-import csv
-
-FILENAME = 'urls_carador_cohe.csv'
-
-
-def create_csv(filename, fieldnames):
-    with open(filename, 'w', encoding='utf-8', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-
-
-def write_csv(filename, data, fieldnames):
-    with open(filename, 'a', encoding='utf-8', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writerow(data)
-
-
-# Отправка запроса на страницу категории
-url = "https://autosolar.es/cargador-coche-electrico"
-response = requests.get(url)
-
-# Проверка успешности запроса
-if response.status_code == 200:
-    # Инициализация объекта BeautifulSoup для парсинга HTML
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # Нахождение всех ссылок на товары
-    product_links = set()
-    link_elements = soup.select('div.category-products a')
-    for link_element in link_elements:
-        link = link_element.get("href")
-        product_links.add(link)
-
-    # Поиск и обработка остальных страниц пагинации
-    pagination = soup.find("div", {'class': 'pagination'})
-    if pagination is not None:
-        pages = pagination.find_all("li")
-        for page in pages:
-            page_link = page.find("a")
-            if page_link is not None:
-                page_url = page_link["href"]
-                page_response = requests.get(page_url)
-                if page_response.status_code == 200:
-                    page_soup = BeautifulSoup(page_response.content, "html.parser")
-                    page_link_elements = page_soup.select('div.category-products a')
-                    for page_link_element in page_link_elements:
-                        page_link = page_link_element.get("href")
-                        product_links.add(page_link)
-
-    # Запись ссылок на товары в файл
-    fieldnames = ['url']
-    create_csv(FILENAME, fieldnames)
-    for link in product_links:
-        data = {'url': link}
-        write_csv(FILENAME, data, fieldnames)
-else:
-    print("Не удалось выполнить запрос к странице категории.")
-
-
