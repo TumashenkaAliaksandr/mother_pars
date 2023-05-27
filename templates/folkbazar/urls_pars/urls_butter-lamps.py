98e5740351eb2c709e0e8585ce5a73572b52cc4c
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-05-27 20:47:32 +0300

    incense check added

--- templates/folkbazar/urls_pars/urls_butter-lamps.py
+++ templates/folkbazar/urls_pars/urls_butter-lamps.py
@@ -26,12 +26,12 @@ if response.status_code == 200:
     # Инициализация объекта BeautifulSoup для парсинга HTML
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Нахождение всех ссылок на товары
+    # Нахождение всех ссылок на товары, исключая ссылки с ключевым словом 'incense'
     product_links = set()
     link_elements = soup.select('div.grid a')
     for link_element in link_elements:
         link = link_element.get("href")
-        if link and '/collections/butter-lamps/products/' in link:
+        if link and '/collections/butter-lamps/products/' in link and 'incense' not in link:
             added = "https://folkbazar.com" + link
             product_links.add(added)
 
@@ -49,7 +49,7 @@ if response.status_code == 200:
                     page_link_elements = page_soup.select('div.page a')
                     for page_link_element in page_link_elements:
                         page_link = page_link_element.get("href")
-                        if page_link and '/collections/butter-lamps/products/' in page_link:
+                        if page_link and ('/collections/butter-lamps/products/' in page_link and 'incense' not in page_link):
                             product_links.add(page_link)
 
     # Запись ссылок на товары в файл
