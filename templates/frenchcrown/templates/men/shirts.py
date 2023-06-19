6793f7246e760ed1c98fcc19972746fb733b7cad
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-06-19 18:56:51 +0300

    collected all the links to the photos

--- templates/frenchcrown/templates/men/shirts.py
+++ templates/frenchcrown/templates/men/shirts.py
@@ -47,22 +47,48 @@ def get_data(url):
     print('Description:', '\n', formatted_text)
 
 
-    image_elements = soup.select('img', {'сlass': 'Image--lazyLoaded'})
-    image_urls = set()  # Множество для хранения уникальных URL-ссылок на изображения
-    for img in image_elements:
-        src_match = re.search(r'src="([^"]+)"', str(img))
-        if src_match:
-            image_url = 'https:' + src_match.group(1)
-            if not image_url.startswith('data:image'):  # Исключаем изображения с URL, начинающимися с 'data:image'
-                image_width = int(img['width']) if 'width' in img.attrs else 0
-                if image_width != 75:  # Исключаем изображения с шириной 75 пикселей
-                    image_urls.add(image_url)
-    print('Изображения:', image_urls)
+    main_image_element = soup.find('img', class_='Image--lazyLoaded')
+    image_urls = set()
 
-    match = re.search(r'v=(\d+)', list(image_urls)[0])
-    if match:
-        id = match.group(1)
-        print("ID:", id)
+    if main_image_element:
+        main_image_url = 'https:' + main_image_element['src']
+        image_url = main_image_url
+        image_urls.add(image_url)
+        print('Главное фото товара:', main_image_url)
+    else:
+        print('Главное фото товара не найдено.')
+
+    if image_urls:
+        match = re.search(r'v=(\d+)', list(image_urls)[0])
+        if match:
+            id = match.group(1)
+            print("ID:", id)
+
+
+    # Находим <div> элемент с классом "Product__Slideshow"
+    slideshow_div = soup.find('div', class_='Product__Slideshow')
+
+    # Если найден <div> элемент с классом "Product__Slideshow"
+    if slideshow_div:
+        # Находим все <div> элементы с классом "Product__SlideItem" внутри слайдшоу
+        slide_items = slideshow_div.find_all('div', class_='Product__SlideItem')
+
+        # Итерируемся по каждому <div> элементу
+        for slide_item in slide_items:
+            # Находим все <img> элементы внутри текущего <div> элемента
+            img_elements = slide_item.find_all('img')
+
+            # Итерируемся по каждому <img> элементу
+            for img_element in img_elements:
+                # Извлекаем значение атрибута src
+                src = 'https:' + img_element['src']
+
+                # Проверяем, что значение атрибута src не содержит "data:image"
+                if 'data:image' not in src:
+                    # Выводим значение атрибута src
+                    print('Фото товара:', src)
+    else:
+        print('Слайдшоу товара не найдено.')
 
     size_title = "Select Size"
     print('size_title:', size_title)
@@ -89,7 +115,7 @@ def get_data(url):
         'price': price,
         'description': formatted_text,
         'category': category,
-        'image_urls': ', '.join(image_urls),
+        'image_urls': src,
         'size_title': size_title,
         'size': labels,
         'color_title': color_title,
