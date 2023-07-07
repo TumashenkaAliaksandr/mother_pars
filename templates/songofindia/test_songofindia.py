78d104c06c6e8b968f6f12850b9a18028d8b2336
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-07-07 21:51:09 +0300

    Corrected the code to get product links with pagination

--- templates/songofindia/test_songofindia.py
+++ templates/songofindia/test_songofindia.py
@@ -1,19 +1,53 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+#
+# url = "https://www.songofindia.co.in/index.php/home-fragrances/reeds-diffuser.html"
+# response = requests.get(url)
+#
+# soup = BeautifulSoup(response.content, "html.parser")
+#
+# product_divs = soup.find_all("div", class_="product photo product-item-photo")
+#
+# links = []
+#
+# for div in product_divs:
+#     link = div.find("a")["href"]
+#     links.append(link)
+#
+# with open("products.csv", "w", newline="") as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerow(["Ссылка на товар"])
+#     for link in links:
+#         writer.writerow([link])
+#
+# print("Ссылки на товары успешно записаны в файл products.csv.")
+
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://www.songofindia.co.in/index.php/home-fragrances/reeds-diffuser.html"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-product_divs = soup.find_all("div", class_="product photo product-item-photo")
-
+base_url = "https://www.songofindia.co.in/index.php/home-fragrances/reeds-diffuser.html?p="
+page_num = 1
 links = []
 
-for div in product_divs:
-    link = div.find("a")["href"]
-    links.append(link)
+while True:
+    url = base_url + str(page_num)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    product_divs = soup.find_all("div", class_="product-item-info")
+
+    for div in product_divs:
+        link = div.find("a")["href"]
+        links.append(link)
+
+    next_link = soup.find("a", class_="next")
+    if not next_link:
+        break
+
+    page_num += 1
 
 with open("products.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
