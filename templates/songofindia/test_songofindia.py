df6a68c7113ac2dd64a4692b1aff1318eafa9bc2
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-07-07 21:31:00 +0300

    test and collected links to products from the home fragrance category

--- templates/songofindia/test_songofindia.py
+++ templates/songofindia/test_songofindia.py
@@ -2,28 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Отправляем GET-запрос к веб-странице
 url = "https://www.songofindia.co.in/index.php/home-fragrances/reeds-diffuser.html"
 response = requests.get(url)
 
-# Создаем объект BeautifulSoup для парсинга HTML-кода страницы
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Находим все элементы <a> с классом "product-name"
-product_links = soup.find_all("a", class_="product-name")
+product_divs = soup.find_all("div", class_="product photo product-item-photo")
 
-# Создаем список для хранения ссылок на товары
 links = []
 
-# Обходим найденные элементы и извлекаем ссылки на товары
-for link in product_links:
-    product_url = link["href"]
-    links.append(product_url)
+for div in product_divs:
+    link = div.find("a")["href"]
+    links.append(link)
 
-# Записываем ссылки в файл CSV
 with open("products.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Ссылка на товар"])  # Записываем заголовок столбца
+    writer.writerow(["Ссылка на товар"])
     for link in links:
         writer.writerow([link])
 
