1a310a9e3da58185abef756ff6124bff7b0ecd46
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-07-11 23:57:44 +0300

    to make sure the links don't repeat.

--- templates/nobero/urls_pars/urls_mens_shorts_collection.py
+++ templates/nobero/urls_pars/urls_mens_shorts_collection.py
@@ -24,12 +24,12 @@ while True:
     last_height = new_height
 
 # Получение ссылок на товары
-product_links = []
+product_links = set()
 link_elements = driver.find_elements('css selector', 'a.product_link')
 for link_element in link_elements:
     link = link_element.get_attribute("href")
     if link and '/products/' in link:
-        product_links.append(link)
+        product_links.add(link)
 
 # Запись ссылок на товары в файл CSV
 fieldnames = ['url']
@@ -39,7 +39,7 @@ with open('templates/../../urls_csv/mens_shorts_collection.csv', 'w', newline=''
     for link in product_links:
         writer.writerow({'url': link})
 
-print("Ссылки на товары были успешно записаны в файл 'men-shorts-collection.csv'.")
+print("Ссылки на товары были успешно записаны в файл 'mens_shorts_collection.csv'.")
 
 # Закрытие веб-драйвера
 driver.quit()
