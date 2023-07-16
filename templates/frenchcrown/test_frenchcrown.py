22de9c5cb6e0eb411cdc5f9e669707a34d991fa7
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-07-16 10:48:34 +0300

    made some minor edits and gave the man_boxers file for checking

--- templates/frenchcrown/test_frenchcrown.py
+++ templates/frenchcrown/test_frenchcrown.py
@@ -1,76 +1,163 @@
-# import requests
-# import csv
-#
-# base_url = "https://hits.gokwik.co/api/v1/header/events/update"
-#
-# def main(base_url):
-#     product_links = set()
-#     page = 1
-#
-#     while True:
-#         url = f"{base_url}?page={page}"
-#         response = requests.get(url)
-#         json_data = response.json()
-#
-#         # Проверяем, есть ли еще страницы
-#         if not json_data or not json_data.get("data"):
-#             break
-#
-#         for item in json_data["data"]:
-#             link = item.get("url")
-#             if link:
-#                 product_links.add(link)
-#
-#         page += 1
-#
-#     with open('urls_boxers.csv', 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['url'])
-#         writer.writerows([[link] for link in product_links])
-#
-#     print("Ссылки на товары были успешно записаны в файл 'urls_boxers.csv'.")
-#
-# main(base_url)
-
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import csv
-
-# Опции для веб-драйвера
-options = Options()
-options.add_argument('--headless')  # Открытие браузера в фоновом режиме
-
-# Инициализация веб-драйвера
-driver = webdriver.Chrome(options=options)
+import time
+from requests import Session
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 base_url = "https://frenchcrown.in/collections/boxers"
-driver.get(base_url)
 
-# Получение ссылок на товары
-product_links = []
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 
-while True:
-    link_elements = driver.find_elements_by_css_selector("div.ProductItem__Info a.pr-title")
-    for link_element in link_elements:
-        link = link_element.get_attribute("href")
-        product_links.append(link)
+def main(base_url):
+    s = Session()
+    s.headers.update(headers)
 
-    next_button = driver.find_element_by_css_selector("div.Pagination__Nav a.is-active + a")
-    if next_button:
-        next_button.click()
-    else:
-        break
+    count = 1
+    pagination = 0
+    is_base_url = True  # Флаг для базового URL
+    start_time = time.time()  # Время начала выполнения
 
-# Запись ссылок на товары в файл CSV
-fieldnames = ['url']
-with open('product_links.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-    for link in product_links:
-        writer.writerow({'url': link})
+    links = []  # Список ссылок
 
-print("Ссылки на товары были успешно записаны в файл 'product_links.csv'.")
+    while True:
 
-# Закрытие веб-драйвера
-driver.quit()
+        if count > 1:
+            url = base_url + "?page=" + str(count)
+        else:
+            url = base_url
+
+        response = s.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+
+        if count == 1:
+            pagination = soup.find('div', class_="ProductListWrapper").find_all("div", class_="jQueryEqualHeight")[-2].get_text(strip=True)
+
+        cards = soup.find_all('div', class_="ProductItem__Info ProductItem__Info--left")
+
+        for card in cards:
+            link_element = card.find("a", target="_blank")
+            link = link_element.get("href")
+            full_link = urljoin(base_url, link)
+            if full_link != base_url:  # Проверка для исключения базового URL
+                links.append(full_link)
+            else:
+                is_base_url = False  # Установка флага в False, если текущая ссылка - базовый URL
+
+        if count == pagination and not is_base_url:
+            break
+
+        count += 1
+
+        # Проверка времени выполнения
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 20:
+            print("Превышено время выполнения. Процесс остановлен.")
+            break
+
+    with open('templates/../../../urls_csv/urls_boxers.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['url'])  # Запись заголовка
+        writer.writerows([[link] for link in links])  # Запись ссылок
+
+    print("Ссылки на товары были успешно записаны в файл 'boxers.csv'.")
+
+
+main(base_url)
+
+
+
+
+
+
+# from requests import Session
+# from bs4 import BeautifulSoup
+# from urllib.parse import urljoin
+#
+# base_url = "https://frenchcrown.in/collections/boxers"
+#
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+# }
+#
+# def main(base_url):
+#     s = Session()
+#     s.headers.update(headers)
+#
+#     count = 1
+#     pagination = 0
+#     is_base_url = True  # Флаг для базового URL
+#     while True:
+#
+#         if count > 1:
+#             url = base_url + "?page=" + str(count)
+#         else:
+#             url = base_url
+#
+#         response = s.get(url)
+#         soup = BeautifulSoup(response.text, "lxml")
+#
+#         if count == 1:
+#             pagination = soup.find('div', class_="ProductListWrapper").find_all("div", class_="jQueryEqualHeight")[-2].get_text(strip=True)
+#
+#         cards = soup.find_all('div', class_="ProductItem__Info ProductItem__Info--left")
+#
+#         for card in cards:
+#             link_element = card.find("a", target="_blank")
+#             link = link_element.get("href")
+#             full_link = urljoin(base_url, link)
+#             if full_link != base_url:  # Проверка для исключения базового URL
+#                 print(full_link)
+#             else:
+#                 is_base_url = False  # Установка флага в False, если текущая ссылка - базовый URL
+#
+#         if count == pagination and not is_base_url:
+#             break
+#
+#         count += 1
+#
+#     with open('boxers.csv', 'w', newline='', encoding='utf-8') as file:
+#         file.write(response.text)
+#
+#     print("Ссылки на товары были успешно записаны в файл 'boxers.html'.")
+#
+#
+# main(base_url)
+#
+
+
+
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+#
+# base_url = "https://frenchcrown.in/collections/boxers"
+#
+# def main(base_url):
+#     product_links = []
+#     data_variants = []
+#
+#     response = requests.get(base_url)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     product_elements = soup.find_all("div", class_="ProductItem__Info")
+#
+#     for product_element in product_elements:
+#         link_element = product_element.find("a", target="_blank")
+#         link = link_element.get("href")
+#         if link and "/products/" in link:
+#             product_links.append(link)
+#             data_variant = product_element.find("span", class_="smartwishlist").get("data-variant")
+#             data_variants.append(data_variant)
+#
+#     with open('product_data.csv', 'w', newline='', encoding='utf-8') as file:
+#         writer = csv.writer(file)
+#         writer.writerow(['url', 'data-variant'])
+#         for link, variant in zip(product_links, data_variants):
+#             writer.writerow([link, variant])
+#
+#     print("Ссылки на товары и значения data-variant были успешно записаны в файл 'product_data.csv'.")
+#
+# main(base_url)
--- templates/frenchcrown/urls_pars/men/urls_boxers.py
+++ templates/frenchcrown/urls_pars/men/urls_boxers.py
@@ -4,11 +4,13 @@ from requests import Session
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+
 base_url = "https://frenchcrown.in/collections/boxers"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
+
 
 def main(base_url):
     s = Session()
@@ -52,112 +54,16 @@ def main(base_url):
 
         # Проверка времени выполнения
         elapsed_time = time.time() - start_time
-        if elapsed_time > 10:
+        if elapsed_time > 20:
             print("Превышено время выполнения. Процесс остановлен.")
             break
 
-    with open('boxers.csv', 'w', newline='', encoding='utf-8') as file:
+    with open('templates/../../../urls_csv/urls_boxers.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['Ссылка на товар'])  # Запись заголовка
+        writer.writerow(['url'])  # Запись заголовка
         writer.writerows([[link] for link in links])  # Запись ссылок
 
     print("Ссылки на товары были успешно записаны в файл 'boxers.csv'.")
 
 
 main(base_url)
-
-
-
-
-
-
-# from requests import Session
-# from bs4 import BeautifulSoup
-# from urllib.parse import urljoin
-#
-# base_url = "https://frenchcrown.in/collections/boxers"
-#
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-# }
-#
-# def main(base_url):
-#     s = Session()
-#     s.headers.update(headers)
-#
-#     count = 1
-#     pagination = 0
-#     is_base_url = True  # Флаг для базового URL
-#     while True:
-#
-#         if count > 1:
-#             url = base_url + "?page=" + str(count)
-#         else:
-#             url = base_url
-#
-#         response = s.get(url)
-#         soup = BeautifulSoup(response.text, "lxml")
-#
-#         if count == 1:
-#             pagination = soup.find('div', class_="ProductListWrapper").find_all("div", class_="jQueryEqualHeight")[-2].get_text(strip=True)
-#
-#         cards = soup.find_all('div', class_="ProductItem__Info ProductItem__Info--left")
-#
-#         for card in cards:
-#             link_element = card.find("a", target="_blank")
-#             link = link_element.get("href")
-#             full_link = urljoin(base_url, link)
-#             if full_link != base_url:  # Проверка для исключения базового URL
-#                 print(full_link)
-#             else:
-#                 is_base_url = False  # Установка флага в False, если текущая ссылка - базовый URL
-#
-#         if count == pagination and not is_base_url:
-#             break
-#
-#         count += 1
-#
-#     with open('boxers.csv', 'w', newline='', encoding='utf-8') as file:
-#         file.write(response.text)
-#
-#     print("Ссылки на товары были успешно записаны в файл 'boxers.html'.")
-#
-#
-# main(base_url)
-#
-
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
-#
-# base_url = "https://frenchcrown.in/collections/boxers"
-#
-# def main(base_url):
-#     product_links = []
-#     data_variants = []
-#
-#     response = requests.get(base_url)
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     product_elements = soup.find_all("div", class_="ProductItem__Info")
-#
-#     for product_element in product_elements:
-#         link_element = product_element.find("a", target="_blank")
-#         link = link_element.get("href")
-#         if link and "/products/" in link:
-#             product_links.append(link)
-#             data_variant = product_element.find("span", class_="smartwishlist").get("data-variant")
-#             data_variants.append(data_variant)
-#
-#     with open('product_data.csv', 'w', newline='', encoding='utf-8') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['url', 'data-variant'])
-#         for link, variant in zip(product_links, data_variants):
-#             writer.writerow([link, variant])
-#
-#     print("Ссылки на товары и значения data-variant были успешно записаны в файл 'product_data.csv'.")
-#
-# main(base_url)
