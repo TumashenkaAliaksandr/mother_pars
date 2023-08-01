c1a3fea1fc7b04763decfe5893801f40a509a8aa
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-08-01 17:39:00 +0300

    added description_all

--- templates/songofindia/test_songofindia.py
+++ templates/songofindia/test_songofindia.py
@@ -269,13 +269,13 @@ import textwrap
 
 
 # Функция для сохранения данных в файл CSV
-def save_to_csv(title, category, price, description, main_image_url, all_image_urls, product_id):
+def save_to_csv(title, category, price, description, description_all, main_image_url, all_image_urls, product_id):
     with open("product_info.csv", "w", newline="", encoding="utf-8") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Title", "Category", "Price", "Description", "Main Image URL", "Other Image URLs", "ID"])
+        writer.writerow(["Title", "Category", "Price", "Description", "Description_all", "Main Image URL", "Other Image URLs", "ID"])
 
         # Записываем данные в строку CSV файла
-        writer.writerow([title, category, price, description, main_image_url, ', '.join(all_image_urls), product_id])
+        writer.writerow([title, category, price, description, description_all, main_image_url, ', '.join(all_image_urls), product_id])
 
     print("Информация о товаре успешно записана в файл product_info.csv.")
 
@@ -325,6 +325,23 @@ print('Price: ', price)
 description = soup.find("div", class_="product attribute overview").find("div", class_="value").text.strip()
 wrapped_description = textwrap.fill(description, width=100, break_long_words=False)
 print('Descriptions: ', wrapped_description)
+
+# Итерироваться по строкам таблицы и собрать текст из всех ячеек в одну переменную
+table = soup.find("table", id="product-attribute-specs-table")
+description_all = ""
+rows = table.find_all("tr")
+for row in rows:
+    # Найти теги <th> и <td> внутри строки
+    th_tags = row.find_all(["th", "td"])
+
+    # Извлечь текст из тегов <th> и <td> с выполнением переноса слов
+    for tag in th_tags:
+        text = tag.text.strip()
+        wrapped_text = textwrap.fill(text, width=100, break_long_words=False)
+        description_all += wrapped_text + "\n"
+        print(wrapped_text)
+    print()
+
 
 # Находим тег div с классом "fotorama__stage__frame" (предполагаем, что это главное фото товара)
 main_image_tag = soup.find("div", class_="fotorama__stage__frame")
@@ -393,7 +410,7 @@ else:
     product_id = "Тег с классом 'fotorama__caption__wrap' не найден."
 
 # Сохраняем данные в файл CSV
-save_to_csv(title, category, price, description, main_image_url, all_large_or_medium_image_urls, product_id)
+save_to_csv(title, category, price, description, description_all, main_image_url, all_large_or_medium_image_urls, product_id)
 
 
 #------------------------------------------------------------------
