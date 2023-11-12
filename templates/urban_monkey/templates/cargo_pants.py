48dbff56724824f21f0d63233de590dc18c5b548
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-11-12 22:10:52 +0300

    added checking and writing to the file

--- templates/urban_monkey/templates/cargo_pants.py
+++ templates/urban_monkey/templates/cargo_pants.py
@@ -1,67 +1,248 @@
-import os
-import re
-import textwrap
+# import os
+# import re
+# import textwrap
+#
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+# import hashlib
+#
+# directory = 'templates/../../done_csv'
+# filename = 'cargo_pants.csv'
+# FILEPARAMS = os.path.join(directory, filename)
+#
+# def create_csv(filename, order):
+#     with open(filename, 'w', encoding='utf-8', newline='') as file:
+#         csv.writer(file).writerow(order)
+#
+# def write_data_csv(filename, data):
+#     with open(filename, 'a', encoding='utf-8', newline='') as file:
+#         csv.DictWriter(file, fieldnames=list(data)).writerow(data)
+#
+# def get_data(url):
+#     html = requests.get(url).text
+#     soup = BeautifulSoup(html, 'html.parser')
+#
+#     title = soup.find("h1", class_="t4s-product__title").text.strip()  # этот кусок кода для тайтла
+#     print('Title: ', title)
+#
+#     category = "Cargo Pants"
+#     sub_category = 'Men’s New Blazers / Trench Coats'
+#     print('Category: ', category)
+#
+#     price = soup.find('div', class_="t4s-product-price").text.replace('Rs.', '').replace(',', '').replace('.00', '').strip()
+#     print('Price: ', price)
+#
+#     # Находим элемент с классом "t4s-tab-wrapper"
+#     tab_wrappers = soup.find_all("div", class_="t4s-tab-wrapper")
+#
+#     # Создаем словарь для хранения текста из каждой вкладки
+#     tab_texts = {}
+#
+#     # Проходим по каждому элементу вкладки
+#     for tab_wrapper in tab_wrappers:
+#         # Находим заголовок вкладки
+#         tab_title = tab_wrapper.find("span", class_="t4s-tab__text").text.strip()
+#
+#         # Находим текст вкладки
+#         tab_content = tab_wrapper.find("div", class_="t4s-tab-content").text.strip()
+#
+#         # Добавляем заголовок и текст в словарь
+#         tab_texts[tab_title] = tab_content
+#
+#     def format_description(description, max_line_length=100):
+#         words = description.split()
+#         lines = []
+#         current_line = []
+#
+#         for word in words:
+#             if len(' '.join(current_line + [word])) <= max_line_length:
+#                 current_line.append(word)
+#             else:
+#                 lines.append(' '.join(current_line))
+#                 current_line = [word]
+#
+#         if current_line:
+#             lines.append(' '.join(current_line))
+#
+#         formatted_description = '\n'.join(lines)
+#         return formatted_description
+#
+#     # Теперь у вас есть доступ к тексту из каждой вкладки по заголовку
+#     core_features_text = (tab_texts.get("core features", "").replace(":", ":\n"))
+#     description_text = tab_texts.get("description", "")
+#     shipping_returns_text = tab_texts.get("shipping & returns", "")
+#     care_guide_text = tab_texts.get("care guide", "")
+#
+#     # Выводим текст из каждой вкладки
+#     print("Core Features:")
+#     print(core_features_text)
+#
+#     print("Description:")
+#     print(description_text)
+#
+#     print("Shipping & Returns:")
+#     print(shipping_returns_text)
+#
+#     print("Care Guide:")
+#     print(care_guide_text)
+#
+#     main_image_element = soup.find('img', class_='Image--lazyLoaded')
+#
+#     # Переменная для хранения найденного id
+#     id = None
+#
+#     if main_image_element:
+#         main_image_url = 'https:' + main_image_element['src']
+#
+#         match = re.search(r'v=(\d+)', main_image_url)
+#         if match:
+#             id = match.group(1)
+#             print("ID:", id)
+#     else:
+#         print('Главное фото товара не найдено.')
+#
+#     # Находим <div> элемент с классом "Product__Slideshow"
+#     slideshow_div = soup.find('div', class_='Product__Slideshow')
+#
+#     # Если найден <div> элемент с классом "Product__Slideshow"
+#     if slideshow_div:
+#         # Инициализируем переменную для хранения списка фото товара
+#         image_urls = []
+#
+#         # Находим все <img> элементы внутри слайдшоу
+#         img_elements = slideshow_div.find_all('img')
+#
+#         # Итерируемся по каждому <img> элементу
+#         for img_element in img_elements:
+#             # Извлекаем значение атрибута src
+#             src = 'https:' + img_element['src']
+#
+#             # Проверяем, что значение атрибута src не содержит "data:image"
+#             if 'data:image' not in src:
+#                 # Добавляем значение атрибута src в список фото товара
+#                 image_urls.append(src)
+#                 print('Фото:', src)
+#
+#     else:
+#         print('Слайдшоу товара не найдено.')
+#
+#     size_title = "Select Size"
+#     print('size_title:', size_title)
+#
+#     label_elements = soup.find('div', class_='Popover__ValueList')
+#     formatted_labels = [label.text.strip() for label in label_elements if label.text.strip()]
+#     labels = '|'.join(formatted_labels)
+#     print(labels)
+#
+#     # color_title = "Select Color"
+#     # print('Title color: ', color_title)
+#     #
+#     # color_vars = soup.find_all('input', class_='color-select-input', attrs={'name': 'color'})
+#     # values = [color_var.get('value') for color_var in color_vars if color_var.get('value')]
+#     # values_str = '|'.join(values)
+#     #
+#     # print('Color:', values_str)
+#
+#
+#     data = {
+#         'title': title,
+#         'price': price,
+#         'story_description': core_features_text,
+#         'category': category,
+#         'sub_category': sub_category,
+#         'image_urls': ', '.join(image_urls),  # Заменил переменную src на image_urls
+#         'size_title': size_title,
+#         'size': labels,
+#         'description': description_text,
+#         'id': id,
+#     }
+#     write_data_csv(FILEPARAMS, data)
+#
+#
+# def main():
+#     order = ['title', 'price', 'story_description', 'category', 'sub_category', 'image_urls', 'size_title', 'size', 'description', 'id']
+#     create_csv(FILEPARAMS, order)
+#     with open('templates/../../urls_csv/urls_urban_monkey.csv', 'r', encoding='utf-8') as file:
+#         for line in csv.DictReader(file):
+#             url = line['url']
+#             get_data(url)
+#
+#
+# if __name__ == '__main__':
+#     main()
 
+
+import os
 import requests
 from bs4 import BeautifulSoup
 import csv
-import hashlib
 
-directory = 'templates/../../done_csv'
-filename = 'cargo_pants.csv'
-FILEPARAMS = os.path.join(directory, filename)
+
+def format_description(description, max_line_length=100):
+    words = description.split()
+    lines = []
+    current_line = []
+
+    for word in words:
+        if len(' '.join(current_line + [word])) <= max_line_length:
+            current_line.append(word)
+        else:
+            lines.append(' '.join(current_line))
+            current_line = [word]
+
+    if current_line:
+        lines.append(' '.join(current_line))
+
+    formatted_description = '\n'.join(lines)
+    return formatted_description
+
 
 def create_csv(filename, order):
     with open(filename, 'w', encoding='utf-8', newline='') as file:
         csv.writer(file).writerow(order)
 
+
 def write_data_csv(filename, data):
     with open(filename, 'a', encoding='utf-8', newline='') as file:
         csv.DictWriter(file, fieldnames=list(data)).writerow(data)
 
-def get_data(url):
+
+def get_data(url, filename):
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
 
-    title = soup.find("h1", class_="t4s-product__title").text.strip()  # этот кусок кода для тайтла
-    print('Title: ', title)
+    title = soup.find("h1", class_="t4s-product__title").text.strip()
+    print('Title:', title)
 
     category = "Cargo Pants"
     sub_category = 'Men’s New Blazers / Trench Coats'
-    print('Category: ', category)
+    print('Category:', category)
 
-    price = soup.find('div', class_="t4s-product-price").text.replace('Rs.', '').replace(',', '').replace('.00', '').strip()
-    print('Price: ', price)
+    price = soup.find('div', class_="t4s-product-price").text.replace('Rs.', '').replace(',', '').replace('.00',
+                                                                                                          '').strip()
+    print('Price:', price)
 
-    # Находим элемент с классом "t4s-tab-wrapper"
     tab_wrappers = soup.find_all("div", class_="t4s-tab-wrapper")
-
-    # Создаем словарь для хранения текста из каждой вкладки
     tab_texts = {}
 
-    # Проходим по каждому элементу вкладки
     for tab_wrapper in tab_wrappers:
-        # Находим заголовок вкладки
         tab_title = tab_wrapper.find("span", class_="t4s-tab__text").text.strip()
-
-        # Находим текст вкладки
         tab_content = tab_wrapper.find("div", class_="t4s-tab-content").text.strip()
-
-        # Добавляем заголовок и текст в словарь
         tab_texts[tab_title] = tab_content
 
-    # Теперь у вас есть доступ к тексту из каждой вкладки по заголовку
-    core_features_text = (tab_texts.get("core features", "").replace(":", ":\n"))
+    core_features_text = tab_texts.get("core features", "").replace(":", ":\n")
     description_text = tab_texts.get("description", "")
     shipping_returns_text = tab_texts.get("shipping & returns", "")
     care_guide_text = tab_texts.get("care guide", "")
 
-    # Выводим текст из каждой вкладки
+    formatted_description = format_description(description_text)
+
     print("Core Features:")
     print(core_features_text)
 
     print("Description:")
-    print(description_text)
+    print(formatted_description)
 
     print("Shipping & Returns:")
     print(shipping_returns_text)
@@ -69,120 +250,31 @@ def get_data(url):
     print("Care Guide:")
     print(care_guide_text)
 
-    # desc_container = soup.find('div', {'class': 'Collapsible Collapsible--large'})
-    # desc = desc_container.text.strip() if desc_container else ''
-    #
-    # lines = []
-    # current_line = ''
-    # for word in desc.split():
-    #     if len(current_line + ' ' + word) > 100:
-    #         lines.append(current_line)
-    #         current_line = word
-    #     else:
-    #         current_line += ' ' + word
-    # if current_line:
-    #     lines.append(current_line)
-    # formatted_text = '\n'.join(lines)
-    # print('Description:', '\n', formatted_text)
-    #
-    # desc_two = ""
-    # # Извлекаем текст из <div class="Rte">
-    # rte_div = soup.find('div', class_='Rte')
-    # if rte_div:
-    #     rte_text = rte_div.get_text(strip=True)
-    #     rte_text = textwrap.fill(rte_text, width=100)
-    #     desc_two += rte_text
-    #
-    # # Извлекаем текст из <div class="product-extra-info">
-    # extra_info_div = soup.find('div', class_='product-extra-info')
-    # if extra_info_div:
-    #     extra_info_items = extra_info_div.find_all('li')
-    #     extra_info_text = '\n'.join([textwrap.fill(item.get_text(strip=True), width=100) for item in extra_info_items])
-    #     desc_two += extra_info_text
-    #
-    # print(desc_two)
-
-    main_image_element = soup.find('img', class_='Image--lazyLoaded')
-
-    # Переменная для хранения найденного id
-    id = None
-
-    if main_image_element:
-        main_image_url = 'https:' + main_image_element['src']
-
-        match = re.search(r'v=(\d+)', main_image_url)
-        if match:
-            id = match.group(1)
-            print("ID:", id)
-    else:
-        print('Главное фото товара не найдено.')
-
-    # Находим <div> элемент с классом "Product__Slideshow"
-    slideshow_div = soup.find('div', class_='Product__Slideshow')
-
-    # Если найден <div> элемент с классом "Product__Slideshow"
-    if slideshow_div:
-        # Инициализируем переменную для хранения списка фото товара
-        image_urls = []
-
-        # Находим все <img> элементы внутри слайдшоу
-        img_elements = slideshow_div.find_all('img')
-
-        # Итерируемся по каждому <img> элементу
-        for img_element in img_elements:
-            # Извлекаем значение атрибута src
-            src = 'https:' + img_element['src']
-
-            # Проверяем, что значение атрибута src не содержит "data:image"
-            if 'data:image' not in src:
-                # Добавляем значение атрибута src в список фото товара
-                image_urls.append(src)
-                print('Фото:', src)
-
-    else:
-        print('Слайдшоу товара не найдено.')
-
-    size_title = "Select Size"
-    print('size_title:', size_title)
-
-    label_elements = soup.find('div', class_='Popover__ValueList')
-    formatted_labels = [label.text.strip() for label in label_elements if label.text.strip()]
-    labels = '|'.join(formatted_labels)
-    print(labels)
-
-    # color_title = "Select Color"
-    # print('Title color: ', color_title)
-    #
-    # color_vars = soup.find_all('input', class_='color-select-input', attrs={'name': 'color'})
-    # values = [color_var.get('value') for color_var in color_vars if color_var.get('value')]
-    # values_str = '|'.join(values)
-    #
-    # print('Color:', values_str)
-
-
+    # Записываем данные в файл CSV
     data = {
-        'title': title,
-        'price': price,
-        'story_description': formatted_text,
-        'category': category,
-        'sub_category': sub_category,
-        'image_urls': ', '.join(image_urls),  # Заменил переменную src на image_urls
-        'size_title': size_title,
-        'size': labels,
-        'description': desc_two,
-        'id': id,
+        "Title": title,
+        "Category": category,
+        "Price": price,
+        "Core Features": core_features_text,
+        "Description": formatted_description,
+        "Shipping & Returns": shipping_returns_text,
+        "Care Guide": care_guide_text
     }
-    write_data_csv(FILEPARAMS, data)
+    write_data_csv(filename, data)
 
 
-def main():
-    order = ['title', 'price', 'story_description', 'category', 'sub_category', 'image_urls', 'size_title', 'size', 'description', 'id']
-    create_csv(FILEPARAMS, order)
-    with open('templates/../../urls_csv/urls_urban_monkey.csv', 'r', encoding='utf-8') as file:
+def process_urls_csv(input_csv, output_csv):
+    create_csv(output_csv,
+               ["Title", "Category", "Price", "Core Features", "Description", "Shipping & Returns", "Care Guide"])
+
+    with open(input_csv, 'r', encoding='utf-8') as file:
         for line in csv.DictReader(file):
             url = line['url']
-            get_data(url)
+            get_data(url, output_csv)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    input_csv = 'templates/../../urls_csv/urls_urban_monkey.csv'
+    output_csv = 'cargo_pants.csv'
+
+    process_urls_csv(input_csv, output_csv)
