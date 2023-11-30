a2b1840dc24dfb7652aa3166a8f634cefba18718
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-11-30 23:56:26 +0300

    added new values Category and writing in the csv file

--- templates/raad_shop/test_raadshop.py
+++ templates/raad_shop/test_raadshop.py
@@ -1,5 +1,6 @@
 # import requests
 # from bs4 import BeautifulSoup
+# import csv
 #
 # url = 'https://raadshop.com/products/hightop-mandalaandroses02869'
 # response = requests.get(url)
@@ -9,12 +10,8 @@
 # header = soup.find("header", class_="mobile-hide")
 # title = header.find("h2", class_="m5 mob-h2").text.strip()
 #
-# print("Title:", title)
-#
 # price_element = soup.find("p", class_="f8pr-price s1pr")
 # current_price = price_element.find("span", class_="cvc-money").find("span", class_="money").text.strip()
-#
-# print("Current Price:", current_price)
 #
 # div_tag = soup.find('div', class_='m6lm')
 #
@@ -24,12 +21,17 @@
 #         text = p_tag.text.strip()
 #         if '•' in text:
 #             text = '\n'.join(line.strip() for line in text.split('•') if line.strip())
-#         print(text)
-#     else:
-#         print("No <p> tag found inside the div.")
-# else:
-#     print("No <div> tag with specified class found.")
-
+#
+# category = 'Shoes'
+#
+# # Запись параметров в CSV-файл
+# with open('done_csv/product_details.csv', mode='w', newline='', encoding='utf-8') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(['Title', 'Current Price', 'Category', 'Description'])
+#     writer.writerow([title, current_price, text if text else 'No description available'])
+#
+#
+#
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -45,8 +47,11 @@ title = header.find("h2", class_="m5 mob-h2").text.strip()
 price_element = soup.find("p", class_="f8pr-price s1pr")
 current_price = price_element.find("span", class_="cvc-money").find("span", class_="money").text.strip()
 
+category = 'Shoes'
+
 div_tag = soup.find('div', class_='m6lm')
 
+text = ''
 if div_tag:
     p_tag = div_tag.find('p')
     if p_tag:
@@ -55,7 +60,7 @@ if div_tag:
             text = '\n'.join(line.strip() for line in text.split('•') if line.strip())
 
 # Запись параметров в CSV-файл
-with open('product_details.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('done_csv/product_details.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['Title', 'Current Price', 'Description'])
-    writer.writerow([title, current_price, text if text else 'No description available'])
+    writer.writerow(['Title', 'Current Price', 'Category', 'Description'])
+    writer.writerow([title, current_price, category, text if text else 'No description available'])
