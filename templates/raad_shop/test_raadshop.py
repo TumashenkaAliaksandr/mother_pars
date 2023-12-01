fc4cdf03378133e88953a9cc8697f6047249fc6b
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-12-01 10:41:07 +0300

    added style colum and for color script, writing all in csv file

--- templates/raad_shop/test_raadshop.py
+++ templates/raad_shop/test_raadshop.py
@@ -1,37 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
-#
-# url = 'https://raadshop.com/products/hightop-mandalaandroses02869'
-# response = requests.get(url)
-#
-# soup = BeautifulSoup(response.content, "html.parser")
-#
-# header = soup.find("header", class_="mobile-hide")
-# title = header.find("h2", class_="m5 mob-h2").text.strip()
-#
-# price_element = soup.find("p", class_="f8pr-price s1pr")
-# current_price = price_element.find("span", class_="cvc-money").find("span", class_="money").text.strip()
-#
-# div_tag = soup.find('div', class_='m6lm')
-#
-# if div_tag:
-#     p_tag = div_tag.find('p')
-#     if p_tag:
-#         text = p_tag.text.strip()
-#         if '•' in text:
-#             text = '\n'.join(line.strip() for line in text.split('•') if line.strip())
-#
-# category = 'Shoes'
-#
-# # Запись параметров в CSV-файл
-# with open('done_csv/product_details.csv', mode='w', newline='', encoding='utf-8') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(['Title', 'Current Price', 'Category', 'Description'])
-#     writer.writerow([title, current_price, text if text else 'No description available'])
-#
-#
-#
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -48,6 +14,13 @@ price_element = soup.find("p", class_="f8pr-price s1pr")
 current_price = price_element.find("span", class_="cvc-money").find("span", class_="money").text.strip()
 
 category = 'Shoes'
+style = "Men's High Tops"
+
+label_element = soup.find("span", class_="data-change-to-option-template--14771537444967__main-product-1")
+
+if label_element:
+    color_sh = label_element.text.strip()
+    print(color_sh)
 
 div_tag = soup.find('div', class_='m6lm')
 
@@ -62,5 +35,5 @@ if div_tag:
 # Запись параметров в CSV-файл
 with open('done_csv/product_details.csv', mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['Title', 'Current Price', 'Category', 'Description'])
-    writer.writerow([title, current_price, category, text if text else 'No description available'])
+    writer.writerow(['Title', 'Current Price', 'Category', 'Style', 'Color', 'Description'])
+    writer.writerow([title, current_price, category, style, color_sh, text if text else 'No description available'])
