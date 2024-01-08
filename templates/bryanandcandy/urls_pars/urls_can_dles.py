24433d6c97cf32393cbe049ba8757d7e91fab2e5
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-01-08 11:24:07 +0300

    Category candles - done

---
+++ templates/bryanandcandy/urls_pars/urls_can_dles.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Получаем HTML-код страницы
+url = 'https://bryanandcandy.com/collections/can-dles'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+product_link = soup.find('a', class_='ProductItem__ImageWrapper')
+
+
+# Находим все ссылки на товары
+product_links = []
+for product in soup.find_all('a', class_='ProductItem__ImageWrapper'):
+    product_link = product.get('href')
+    full_link = f'https://bryanandcandy.com{product_link}'
+    product_links.append(full_link)
+
+# Выводим все найденные ссылки
+for link in product_links:
+    print(link)
+
+
+# Записываем ссылки в CSV файл
+with open('../urls_csv/can_dles_links.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['url'])
+    for link in product_links:
+        writer.writerow([link])
