4f2a5ffef0170ed4cdf19a4b406dfe3337fed43c
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-01-08 11:33:02 +0300

    Category coffee - done

---
+++ templates/bryanandcandy/templates/coffee_products.py
@@ -0,0 +1,62 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import csv
+import random
+import string
+
+# Функция для генерации уникального числового ID
+def generate_numeric_id(length=10):
+    return ''.join(random.choices(string.digits, k=length))
+
+# Функция для получения ID с веб-страницы
+def get_product_id(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Генерация уникального числового ID для товара
+    return generate_numeric_id()
+
+# Чтение ссылок из CSV файла и обработка
+input_file = '../urls_csv/coffee_products_links.csv'
+output_file = '../done_csv/coffee_products.csv'
+
+with open(input_file, mode='r', newline='', encoding='utf-8') as file:
+    reader = csv.reader(file)
+    next(reader)  # Пропуск заголовков, если они есть
+
+    with open(output_file, mode='w', newline='', encoding='utf-8') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(['Title', 'Cost', 'Description', 'Photos', 'Brand', 'Сategory', 'id'])
+
+        for row in reader:
+            url = row[0]  # Получаем ссылку на товар из столбца CSV файла
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Проверяем наличие информации о товаре
+            title = soup.find('h1', class_='ProductMeta__Title')
+            cost = soup.find('span', class_='ProductMeta__Price')
+            description = soup.find('div', class_='ProductMeta__Description')
+
+            if title and cost and description:
+                title_text = title.text.strip()
+                cost_text = cost.text.strip().replace('Rs.', '')
+                description_text = description.text.strip()
+                brand = 'Bryan & Candy'
+                category = "Coffee"
+
+                photos = []
+                for link in soup.find_all('a', class_='Product__SlideshowNavImage'):
+                    href = link.get('href')
+                    if href.startswith('//'):
+                        href = 'https:' + href  # Добавляем 'https:' к ссылке, если она начинается с '//'
+                    if len(photos) < 3:  # Ограничиваем количество фото тремя
+                        photos.append(href)  # Добавляем только ссылку на фото
+
+                section_id = get_product_id(url)  # Получаем ID продукта
+                writer.writerow([title_text, cost_text, description_text, '\n'.join(photos), brand, category, section_id])
+            else:
+                writer.writerow(['', '', '', '', '', '', 'ID продукта не найден или информация о товаре отсутствует'])
+
+print('Информация успешно записана в coffee_products.csv')
