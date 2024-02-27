05e4c50d832b51897be666d05b7f0d203f38f2ff
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-02-27 23:57:17 +0300

    Fixed the fields for the record

--- templates/groovee/tests/template_test.py
+++ templates/groovee/tests/template_test.py
@@ -4,6 +4,11 @@ import string
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+# Записываем заголовки столбцов в CSV файл
+with open('groovee_product_details.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Product ID', 'Brand', 'Category', 'Solid By', 'Title', 'Price', 'Sizes', 'Description', 'Photo URLs', 'URL_Product'])
+
 # Чтение URL-адреса товара из CSV файла
 with open('../urls_csv/urls_groovee_tsh.csv', 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
@@ -26,9 +31,16 @@ with open('../urls_csv/urls_groovee_tsh.csv', 'r', newline='', encoding='utf-8')
 
         # Извлекаем данные о товаре
         title = soup.find('div', {'class': 'product-detail'}).find('h1', recursive=False).text.strip()
-        price = soup.find('del', class_='text-danger').text.strip().replace('₹', '')
+        price_element = soup.find('del', class_='text-danger')
+        if price_element:
+            price = price_element.text.strip().replace('₹', '')
+        else:
+            price = 'Out of stock'
         sizes_container = soup.find('div', {'class': 'option-values d-flex flex-flow flex-wrap'})
-        sizes = '|'.join([size.text.strip() for size in sizes_container.find_all('a', {'role': 'button'})])
+        if sizes_container:
+            sizes = '|'.join([size.text.strip() for size in sizes_container.find_all('a', {'role': 'button'})])
+        else:
+            sizes = 'Out of stock'
         description_block = soup.find('div', class_='description')
         descriptions = '\n'.join([description.text.strip() for description in description_block.find_all('p')])
         brand = 'Groovee'
@@ -44,10 +56,9 @@ with open('../urls_csv/urls_groovee_tsh.csv', 'r', newline='', encoding='utf-8')
             if len(best_photo_urls) == 2:  # Прекращаем поиск после того, как найдены 2 уникальные ссылки
                 break
 
-        # Создаем CSV файл и записываем данные
+        # Добавляем данные в CSV файл
         with open('groovee_product_details.csv', 'a', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Product ID', 'Brand', 'Category', 'Solid By', 'Title', 'Price', 'Sizes', 'Description', 'Photo URLs', 'URL_Product'])
             writer.writerow([product_id, brand, category, sub_category, title, price, sizes, descriptions, ', '.join(best_photo_urls), url])
 
         # Закрываем браузер после использования
