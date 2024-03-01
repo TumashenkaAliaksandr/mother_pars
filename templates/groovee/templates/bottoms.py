7d381d6927fd63b7cc9283f7cd510c4fd9ee5da9
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-03-01 23:11:56 +0300

    added new template for parsing the Bottoms - done

---
+++ templates/groovee/templates/bottoms.py
@@ -0,0 +1,70 @@
+import csv
+import random
+import string
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+# Записываем заголовки столбцов в CSV файл
+with open('../done_csv/groovee_bottoms.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Product ID', 'Brand', 'Category', 'Solid By', 'Title', 'Price', 'Sizes', 'Description', 'Photo URLs', 'URL_Product'])
+
+# Чтение URL-адреса товара из CSV файла
+with open('../urls_csv/urls_bottoms.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Пропускаем заголовки
+    for row in reader:
+        url = row[0]  # Получаем URL-адрес из первого столбца (первый столбец содержит URL)
+
+        # Генерируем случайный ID из цифр
+        product_id = ''.join(random.choices(string.digits, k=8))
+
+        # Запускаем браузер
+        driver = webdriver.Chrome()
+        driver.get(url)
+
+        # Получаем HTML-код страницы после загрузки JavaScript
+        html = driver.page_source
+
+        # Используем BeautifulSoup для парсинга HTML
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Извлекаем данные о товаре
+        title = soup.find('div', {'class': 'product-detail'}).find('h1', recursive=False).text.strip()
+        price_element = soup.find('del', class_='text-danger')
+        if price_element:
+            price = price_element.text.strip().replace('₹', '')
+        else:
+            price = 'Out of stock'
+        sizes_container = soup.find('div', {'class': 'option-values d-flex flex-flow flex-wrap'})
+        if sizes_container:
+            sizes = '|'.join([size.text.strip() for size in sizes_container.find_all('a', {'role': 'button'})])
+        else:
+            sizes = 'Out of stock'
+        description_block = soup.find('div', class_='description')
+        descriptions = '\n'.join([description.text.strip() for description in description_block.find_all('p')])
+        brand = 'Groovee'
+        category = 'Bottoms'
+        # Извлекаем подкатегорию товара
+        sub_category_element = soup.find('div', {'class': 'product-brand-wrap mb-2'})
+        if sub_category_element:
+            sub_category = sub_category_element.find('span', recursive=False).text.strip()
+        else:
+            sub_category = 'N/A'
+
+        # Извлекаем URL-адреса лучших фотографий товара (размером 1800)
+        photo_gallery = soup.find_all('img', class_='feature-image')
+        best_photo_urls = set()  # Множество для уникальных ссылок
+        for photo in photo_gallery:
+            if '1800' in photo['src']:
+                best_photo_urls.add(photo['src'])
+            if len(best_photo_urls) == 2:  # Прекращаем поиск после того, как найдены 2 уникальные ссылки
+                break
+
+        # Добавляем данные в CSV файл
+        with open('../done_csv/groovee_bottoms.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([product_id, brand, category, sub_category, title, price, sizes, descriptions, ', '.join(best_photo_urls), url])
+
+        # Закрываем браузер после использования
+        driver.quit()
---
+++ templates/groovee/urls_pars/urls_bottoms.py
@@ -0,0 +1,35 @@
+import csv
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import urllib.parse
+
+url = "https://www.groovee.in/browse/trousers-chinos-jeans-streetwear-loose-oversize-cargo-for-men"
+
+# Запускаем браузер
+driver = webdriver.Chrome()
+driver.get(url)
+
+# Получаем HTML-код страницы после загрузки JavaScript
+html = driver.page_source
+
+# Используем BeautifulSoup для парсинга HTML
+soup = BeautifulSoup(html, 'html.parser')
+
+# Находим все ссылки на странице, которые начинаются с '/product/'
+links = soup.find_all('a', href=True)
+product_links = set()
+for link in links:
+    if link['href'].startswith('/product/'):
+        product_links.add(urllib.parse.urljoin(url, link['href']))
+
+# Записываем найденные ссылки в файл CSV
+with open('../urls_csv/urls_bottoms.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Link']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for product_link in product_links:
+        writer.writerow({'Link': product_link})
+
+# Закрываем браузер после использования
+driver.quit()
