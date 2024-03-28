df5b28e617eb2594178a0d932439ede3eddca930
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-03-28 17:02:59 +0300

    finded image

--- templates/chokore/tests_templates/test_template_chokore.py
+++ templates/chokore/tests_templates/test_template_chokore.py
@@ -34,6 +34,37 @@ def scrape_product_data(product_id):
     details = soup.find('div', class_='so-tab-content cust_tab2_conTent').text.strip()
     print('Datails:', details)
 
+    # Находим все изображения
+    image_wrappers = soup.find_all('div', class_='responsive-image__wrapper')
+
+    # Создаем список для хранения ссылок на изображения
+    images = []
+
+    # Проходим по каждому блоку с изображением
+    for image_wrapper in image_wrappers:
+        # Получаем ссылку на изображение
+        image_src = image_wrapper.img['src'].lstrip('//')
+        # Получаем размеры изображения
+        image_sizes = image_wrapper.img['data-widths']
+        # Разбиваем строку размеров и преобразуем в список чисел
+        image_sizes = [int(size.strip()) for size in image_sizes[1:-1].split(',')]
+        # Находим максимальный размер
+        max_size = max(image_sizes)
+        # Добавляем ссылку и максимальный размер в список
+        images.append((image_src, max_size))
+
+    # Сортируем список изображений по размеру в обратном порядке
+    images.sort(key=lambda x: x[1], reverse=True)
+
+    # Выбираем три изображения с наибольшим размером
+    top_three_images = images[:3]
+
+    # Выводим результат
+    for index, (image_src, image_size) in enumerate(top_three_images, 1):
+        print(f"Изображение {index}:")
+        print(f"Ссылка: {image_src}")
+        print(f"Размер: {image_size}")
+
     # Находим фото (photo) - берем первые три фото
     photo_elements = soup.find_all('img', class_='product-single__photo')
     photos = [photo['src'] for photo in photo_elements[:3]]
