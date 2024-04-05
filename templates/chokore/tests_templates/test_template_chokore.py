2f12165f98c0cd97face61d4fcedbe9f4ae491f0
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-04-05 09:19:30 +0300

    chokore - gave csv file to for inspection - a category for men

--- templates/chokore/tests_templates/test_template_chokore.py
+++ templates/chokore/tests_templates/test_template_chokore.py
@@ -1,113 +1,82 @@
-import requests
-from bs4 import BeautifulSoup
 import csv
 import random
+import string
+import requests
+from bs4 import BeautifulSoup
+import time
+
+# Записываем заголовки столбцов в CSV файл
+with open('chokore_product_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Product ID', 'Title', 'Price', 'Description', 'Details', 'Max Image URL', 'Category', 'URL'])
 
 # Функция для получения случайного товарного ID
-def get_random_product_id():
-    # Ваша логика для получения случайного ID товара
-    return 'chokore-vintage-cowboy-hat-chocolate-brown'
+def generate_product_id():
+    return ''.join(random.choices(string.digits, k=8))
 
 # Функция для извлечения данных с карточки товара
-def scrape_product_data(product_id):
-    url = f'https://www.chokore.com/collections/mens-accessories/products/{product_id}'
+def scrape_product_data(url):
+    # Генерируем случайный ID из цифр
+    product_id = generate_product_id()
+
+    # Отправляем GET-запрос на страницу товара
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # Находим заголовок (title)
     title = soup.find('div', {'class': 'custom-title-wishlist'}).find('h1', recursive=False).text.strip()
-    print('Title:', title)
 
     # Находим цену (price)
     price_element = soup.find('s', {'class': 'product-single__sale-price'}).find('span', recursive=False).text.strip()
-    if price_element:
-        price = price_element.strip().replace('₹', '')
-    else:
-        price = 'Out of stock'
-    print('Price:', price)
+    price = price_element.strip().replace('₹', '') if price_element else 'Out of stock'
 
     # Находим описание (description)
     description = soup.find('div', class_='so-tab-content cust-tab4-content').text.strip()
-    print('Description:', description)
 
     # Находим детали (details)
     details = soup.find('div', class_='so-tab-content cust_tab2_conTent').text.strip()
-    print('Datails:', details)
 
     # Находим все изображения
-    image_wrappers = soup.find_all('div', class_='responsive-image__wrapper')
+    image_wrappers = soup.find_all('a', class_='chocolat-image')
+    max_image_url = image_wrappers[0]['href'].lstrip('//')
 
-    # Создаем список для хранения ссылок на изображения
-    images = []
-
-    # Проходим по каждому блоку с изображением
-    for image_wrapper in image_wrappers:
-        # Получаем ссылку на изображение
-        image_src = image_wrapper.img['src'].lstrip('//')
-        # Получаем размеры изображения
-        image_sizes = image_wrapper.img['data-widths']
-        # Разбиваем строку размеров и преобразуем в список чисел
-        image_sizes = [int(size.strip()) for size in image_sizes[1:-1].split(',')]
-        # Находим максимальный размер
-        max_size = max(image_sizes)
-        # Добавляем ссылку и максимальный размер в список
-        images.append((image_src, max_size))
-
-    # Сортируем список изображений по размеру в обратном порядке
-    images.sort(key=lambda x: x[1], reverse=True)
-
-    # Выбираем три изображения с наибольшим размером
-    top_three_images = images[:3]
-
-    # Выводим результат
-    for index, (image_src, image_size) in enumerate(top_three_images, 1):
-        print(f"Изображение {index}:")
-        print(f"Ссылка: {image_src}")
-        print(f"Размер: {image_size}")
-
-    # Находим фото (photo) - берем первые три фото
-    photo_elements = soup.find_all('img', class_='product-single__photo')
-    photos = [photo['src'] for photo in photo_elements[:3]]
-
-    # Находим категорию (category) и подкатегорию (sub category)
-    breadcrumbs = soup.find('span', class_='breadcrumbs__link').text.strip()
-    print('Breadcrumb:', breadcrumbs)
-    category = breadcrumbs[-2].text.strip()
-    sub_category = breadcrumbs[-1].text.strip()
+    # Находим категорию (category)
+    breadcrumbs = soup.find('li', class_='breadcrumbs__item category').text.strip()
 
     return {
+        'product_id': product_id,
         'title': title,
         'price': price,
         'description': description,
         'details': details,
-        'photos': photos,
-        'category': category,
-        'sub_category': sub_category,
+        'max_image_url': max_image_url,
+        'category': breadcrumbs,
         'url': url
     }
 
-# Получаем случайный товарный ID
-product_id = get_random_product_id()
+# Чтение URL-адреса товара из CSV файла
+with open('../urls_csv/product_links.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Пропускаем заголовки
+    for row in reader:
+        url = row[0]  # Получаем URL-адрес из первого столбца
 
-# Извлекаем данные с карточки товара
-product_data = scrape_product_data(product_id)
+        # Извлекаем данные с карточки товара
+        product_data = scrape_product_data(url)
 
-# Записываем данные в CSV файл
-with open('product_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['title', 'price', 'description', 'details', 'photo1', 'photo2', 'photo3', 'category', 'sub_category', 'url']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow({
-        'title': product_data['title'],
-        'price': product_data['price'],
-        'description': product_data['description'],
-        'details': product_data['details'],
-        'photo1': product_data['photos'][0] if len(product_data['photos']) > 0 else '',
-        'photo2': product_data['photos'][1] if len(product_data['photos']) > 1 else '',
-        'photo3': product_data['photos'][2] if len(product_data['photos']) > 2 else '',
-        'category': product_data['category'],
-        'sub_category': product_data['sub_category'],
-        'url': product_data['url']
-    })
+        # Записываем данные в CSV файл
+        with open('chokore_product_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([
+                product_data['product_id'],
+                product_data['title'],
+                product_data['price'],
+                product_data['description'],
+                product_data['details'],
+                product_data['max_image_url'],
+                product_data['category'],
+                product_data['url']
+            ])
 
-print("Данные успешно записаны в файл 'product_data.csv'")
+        # Задержка, чтобы не нагружать сервер
+        time.sleep(3)  # Подождите 3 секунды между запросами
